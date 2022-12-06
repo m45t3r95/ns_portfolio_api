20e95cb4167792be7770ce0e919ab772b92ae837
Master Hacker <n33r4j95@gmail.com>
2022-12-06 19:14:10 +0530

    commit 1

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
